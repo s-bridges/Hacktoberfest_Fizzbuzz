fe087091c79ee8302219e9047d2c612c1fbb559a
Manuel Lang <manuellang183@gmail.com>
2019-10-01 20:40:01 +0200

    fixed range in python script

--- Challenge/Python/manu183.py
+++ Challenge/Python/manu183.py
@@ -1,1 +1,1 @@
-print('\n'.join(['Spider' if i%15==0 else 'Ghost' if i%5==0 else 'Rat' if i%3==0 else str(i) for i in range(1,100)]))
+print('\n'.join(['Spider' if i%15==0 else 'Ghost' if i%5==0 else 'Rat' if i%3==0 else str(i) for i in range(1,101)]))

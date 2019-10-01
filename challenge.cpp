5c3e65710ccca9fd6e6c92a14a36bad71405f9c0
NITIN AGGARWAL <45655770+nitin-1906@users.noreply.github.com>
2019-10-01 21:31:57 +0530

    Implemented challenge.cpp

---
+++ challenge.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+  for(int i=1;i<=100;i++)
+  {
+    if(i%15==0)
+    cout<<"Spider"<<endl;
+    else if(i%3==0)
+    cout<<"Rat"<<endl;
+    else if(i%5==0)
+    cout<<"Ghost"<<endl;
+    else cout<<i<<endl;
+  }
+  return 0;
+}

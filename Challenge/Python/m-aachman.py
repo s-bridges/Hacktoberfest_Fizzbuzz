3d03a9c62d40e25f49bc040ac0e906992c0a3cc0
m-aachman <53348462+m-aachman@users.noreply.github.com>
2019-10-01 23:53:08 +0530

    Create m-aachman.py

---
+++ Challenge/Python/m-aachman.py
@@ -0,0 +1,10 @@
+# https://github.com/m-aachman
+for i in range(1,101):
+    if i%15==0:
+        print("Spider")
+    elif i%3==0:
+        print("Rat")
+    elif i%5==0:
+        print("Ghost")
+    else:
+        print(i)

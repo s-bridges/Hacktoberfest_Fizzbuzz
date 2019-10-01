db41659221c58199c6d378b3480662e6e8b845df
David Gokimmung <43725247+Davidgokimmung@users.noreply.github.com>
2019-10-01 22:15:50 +0530

    Add files via upload

---
+++ Challenge/Davidgokimmung.cpp
@@ -0,0 +1,9 @@
+//https://github.com/Davidgokimmung
+#include<iostream>
+using namespace std;
+int main(){
+    for(int i=1;i<=100;i++){
+        string x = i%3==0 && i%5==0 ? "Spider" : i%3==0 ? "Rat" : i%5==0 ? "Ghost" : "NULL";
+        cout<<(x=="NULL"?to_string(i):x)<<endl;
+    }
+}

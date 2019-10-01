11f3bb923593ede7b05433123ce9d125380d75ff
derNiklaas <derNiklaas@users.noreply.github.com>
2019-10-01 18:28:16 +0200

    I wrote this in java

---
+++ Challenge/derNiklaas.java
@@ -0,0 +1,15 @@
+public class derNiklaas {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("Spider");
+            } else if (i % 3 == 0) {
+                System.out.println("Rat");
+            } else if (i % 5 == 0) {
+                System.out.println("Ghost");
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+}

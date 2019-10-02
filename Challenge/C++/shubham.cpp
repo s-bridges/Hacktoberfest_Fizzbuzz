d797d66adf63563f0dee04f619ccd885cb428a73
shubham14173 <56061645+shubham14173@users.noreply.github.com>
2019-10-02 11:27:22 +0530

    i wrote this in c++

---
+++ Challenge/C++/shubham.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    for int(i=1;i<100;i++)
+    {
+        if(i%3==0&&i%5==0)
+        cout<<"Spider"<<endl;
+        if(i%3==0)
+        cout<<"Rat"<<endl;
+        else if(i%5==0)
+        cout<<"Ghost"<<endl;
+    } 
+}

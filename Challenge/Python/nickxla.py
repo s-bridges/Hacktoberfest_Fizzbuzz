b49e09c9530e497c092ec3791cf7fe16cfe5c050
Nicolas PICARD <nicolaspicard@protonmail.com>
2019-10-05 13:05:33 +0200

    Added python nickxla

---
+++ Challenge/Python/nickxla.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+for i in range(1,100):
+    if i % 3 == 0 and i % 5 == 0:
+        print('Spider')
+    elif i % 3 == 0:
+        print('Rat')
+    elif i % 5 == 0:
+        print('Ghost')
+    else:
+        print(i)

cb723cc5f2c8438690e885322bc85ebe3a9c5c00
Boot-Error <booterror99@gmail.com>
2019-10-12 10:54:23 +0530

    I wrote this in Go

---
+++ Challenge/Go/Boot-Error.go
@@ -0,0 +1,19 @@
+// https://github.com/Boot-Error/
+
+package main
+
+import "fmt"
+
+func main() {
+
+	for i := 0; i <= 100; i++ {
+
+		if i%3 == 0 && i%5 == 0 {
+			fmt.Println("FizzBuzz")
+		} else if i%3 == 0 {
+			fmt.Println("Fizz")
+		} else if i%5 == 0 {
+			fmt.Println("Buzz")
+		}
+	}
+}

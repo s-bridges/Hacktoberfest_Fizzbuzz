1f9c1310ba96ce1dea430e46a6689067b99dc728
Tayyab Yasin <tayyibyasin786@gmail.com>
2019-10-27 15:56:00 +0500

    I wrote this in PHP

---
+++ Challenge/PHP/Tayyibyasin7860.php
@@ -0,0 +1,16 @@
+<?php
+//https://github.com/Tayyibyasin7860
+$times = 100;
+for($i = 1; $i <= $times; $i++){
+    if($i % 3 == 0 ){
+        if($i % 5 == 0){
+            echo "Spider"."</br>";
+        }
+        else {
+            echo "Rat"."</br>";
+        }
+    }elseif($i % 5 == 0){
+        echo "Ghost"."</br>";
+    }else{
+        echo $i."</br>";
+    }
